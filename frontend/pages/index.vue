a9f4adcb4ff5ba8decb9c85d04f9982135c9b639
DESKTOP-8JJ7F6C\Firdaus Riyanto <firdausriyanto@gmail.com>
2021-11-12 07:26:32 +0700

    initial commit frontend

---
+++ frontend/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    {{ posts }}
+  </div>
+</template>
+
+<script>
+import postsQuery from '~/graphql/queries/post/posts'
+
+export default {
+  apollo: {
+    posts: {
+      prefetch: true,
+      query: postsQuery,
+      variables() {
+        return { id: parseInt(this.$route.params.id) }
+      },
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ frontend/pages/posts/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ post }}
+  </div>
+</template>
+
+<script>
+import postQuery from '~/graphql/queries/post/post'
+
+export default {
+  apollo: {
+    post: {
+      prefetch: true,
+      query: postQuery,
+      variables() {
+        return { id: parseInt(this.$route.params.id) }
+      },
+    },
+  },
+}
+</script>
